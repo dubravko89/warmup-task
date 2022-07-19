936ee0090c361f0c78145515c49470f72cc1a4a7
Dubravko Stefancic <dubravko.stefancic89@gmail.com>
2022-07-19 17:33:35 +0200

    zadnje promjene

--- grades.php
+++ grades.php
@@ -1,16 +1,12 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Grades</title>
 </head>
-<body>
-
-</body>
-</html>
 <?php
-$perc = 68;
+$perc = $_POST['percentage'];
 if ($perc <= 100) {
 switch ($perc) {
 	case $perc >= 90 : $grad = "Excellent"; break;
@@ -18,5 +14,19 @@ switch ($perc) {
 	case $perc >= 50 : $grad = "Good"; break;
 	default : $grad = "Try again"; break;
 };} else echo "Wrong input";
-echo $grad;
+
 ?>
+<body>
+	<form action="" method="post" id="grader">
+		<p>
+			<label>Enter the percentage:</label>
+			<input type="number" name="percentage" value="<?php echo $perc; ?>">
+			<input type="submit" name="submit">
+		</p>
+		<p>
+			<label>Grade:</label>
+			<input readonly="readonly" name="grade" value="<?php echo $grad; ?>">
+		</p>
+	</form>
+</body>
+</html>
